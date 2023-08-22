b923584c7c396186fefbb3840a1d4aba46af98d5
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-08-22 22:52:47 +0200

    Add: routes validation using celebrate & Joi

--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,31 @@
 const router = require('express').Router();
+const { URL_REGEX } = require('../utils/regex')
 const { celebrate, Joi } = require('celebrate')
-
 const { getMovies, addMovieToFavorites, deleteMovieFromFavorites } = require('../controllers/users')
 // GET: Movies From user's Favorites
 router.get('/', getMovies)
 // POST: Add Movie To Favorites
-router.post('/', addMovieToFavorites)
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(URL_REGEX),
+    trailerLink: Joi.string().required().pattern(URL_REGEX),
+    thumbnail: Joi.string().required().pattern(URL_REGEX),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  })
+}) ,addMovieToFavorites)
 // DELETE: Remove Movie From Favorites
-router.delete('/:movieId', deleteMovieFromFavorites)
+router.delete('/:_id', celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().length(24).hex().required()
+  })
+}), deleteMovieFromFavorites)
 
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,11 @@ const { getCurrentUser, updateUser } = require('../controllers/users')
 // GET: Current User
 router.get('/me', getCurrentUser);
 // POST: Update User's name and email
-router.patch('/me', updateUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email()
+  })
+}), updateUser);
 
 module.exports = router
