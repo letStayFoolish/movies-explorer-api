a81e7cfd60b8e05e69b307af78a7a30e7c566b3b
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-08-23 22:05:01 +0200

    Update: .then & .catch replaced with async/await

--- controllers/movies.js
+++ controllers/movies.js
@@ -6,88 +6,94 @@ const RequestForbidden403 = require("../error-handlers/request-forbidden-403");
 
 // Get All User's Movies:
 // @todo need to get only movies added by current user!!! For now, get all movies from the server
-const getMovies = (req, res, next) => {
-  return Movie.find({})
-    .then((movies) => {
-      // Status 200:
-      res.send(movies)
-    })
-    .catch(next)
+const getMovies = async (req, res, next) => {
+  try {
+    const movies = await Movie.find({})
+
+    // Status 200:
+    res.send(movies)
+
+  } catch (error) {
+    next(error)
+  }
 }
 
 // Post Movies:
-const addMovieToFavorites = (req, res, next) => {
-  const {
-    country,
-    director,
-    duration,
-    year,
-    description,
-    image,
-    trailerLink,
-    thumbnail,
-    movieId,
-    nameRU,
-    nameEN,
-  } = req.body
-  const owner = req.user._id
+const addMovieToFavorites = async (req, res, next) => {
+  try {
+    const {
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      thumbnail,
+      movieId,
+      nameRU,
+      nameEN,
+    } = req.body
+    const owner = req.user._id
 
-  // Add new movie to the database:
-  return Movie.create({
-    country,
-    director,
-    duration,
-    year,
-    description,
-    image,
-    trailerLink,
-    thumbnail,
-    owner,
-    movieId,
-    nameRU,
-    nameEN,
-  })
-    .then((movie) => {
-      // Status 201:
-      res.send(movie)
+    // Add new movie to the database:
+    const movie = await Movie.create({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      thumbnail,
+      owner,
+      movieId,
+      nameRU,
+      nameEN,
     })
-    .catch((error) => {
-      if (error.name === 'ValidationError') {
-        // Status 400:
-        return next(new BadRequest400('Переданы некорректные данные при добавлении фильма в избранное.'))
-      }
-      // Status 500:
-      return next(error)
-    })
+
+    // Status 201:
+    res.send(movie)
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      // Status 400:
+      return next(new BadRequest400('Переданы некорректные данные при добавлении фильма в избранное.'))
+    }
+    // Status 500:
+    return next(error)
+  }
 }
 
 // Delete Movie(id):
-const deleteMovieFromFavorites = (req, res, next) => {
-  const { _id } = req.params
+const deleteMovieFromFavorites = async (req, res, next) => {
+  try {
+    const { _id } = req.params
 
-  return Movie.findById(_id)
-    .then((movie) => {
-      if (!movie) {
-        // Status 404:
-        throw new NotFound404(`Фильм с указанным _id: ${_id} не найден.`)
-      }
-      const owner = String(movie.owner)
-      if (owner !== req.user._id) {
-        throw new RequestForbidden403('Нет прав для удаления данного фильма.')
-      }
-      return movie.deleteOne()
-    })
-    .then((movie) => {
-      res.send({ movie })
-    })
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        // Status 400:
-        return next(new BadRequest400('Переданы некорректные данные для удаления фильма из избранного.'))
-      }
-      // Status 500:
-      return next(error)
-    })
+    const movie = await Movie.findById(_id)
+
+    if (!movie) {
+      // Status 404:
+      throw new NotFound404(`Фильм с указанным _id: ${_id} не найден.`)
+    }
+
+    const owner = String(movie.owner)
+
+    if (owner !== req.user._id) {
+      throw new RequestForbidden403('Нет прав для удаления данного фильма.')
+    }
+
+    await movie.deleteOne()
+
+    res.send({ movie })
+  } catch (error) {
+    if (error.name === 'CastError') {
+      // Status 400:
+      return next(new BadRequest400('Переданы некорректные данные для удаления фильма из избранного.'))
+    }
+
+    // Status 500:
+    return next(error)
+  }
 }
 
 module.exports = { getMovies, addMovieToFavorites, deleteMovieFromFavorites }
--- controllers/users.js
+++ controllers/users.js
@@ -3,55 +3,59 @@ const BadRequest400 = require('../error-handlers/bad-request-400')
 const NotFound404 = require('../error-handlers/not-found-404')
 const RequestConflict409 = require('../error-handlers/request-conflict-409')
 // Get Current User
-const getCurrentUser = (req, res, next) => {
-  const userId = req.user._id
+const getCurrentUser = async (req, res, next) => {
+  try {
+    const userId = req.user._id
 
-  return User.findById(userId)
-    .then((user) => {
-      if (!user) {
-        // Status 404:
-        throw new NotFound404(`Пользователь по указанному id: ${userId} не найден.`)
-      }
-      // Status 200:
-      res.json(user)
-    })
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        // Status 400:
-        return next(new BadRequest400('Указан некорректный id.'))
-      }
-      // Status 500:
-      return next(error)
-    })
+    const user = await User.findById(userId)
+
+    if (!user) {
+      // Status 404:
+      throw new NotFound404(`Пользователь по указанному id: ${userId} не найден.`)
+    }
+    // Status 200:
+    res.json(user)
+
+  } catch (error) {
+    if (error.name === 'CastError') {
+      // Status 400:
+      return next(new BadRequest400('Указан некорректный id.'))
+    }
+    // Status 500:
+    return next(error)
+  }
 }
 
-const updateUser = (req, res, next) => {
-  const {name, email} = req.body
-  const updData = {
-    name,
-    email,
+const updateUser = async (req, res, next) => {
+  try {
+    const {name, email} = req.body
+    const updData = {
+      name,
+      email,
+    }
+
+    const userId = req.user._id
+
+    const user = await User.findByIdAndUpdate(userId, updData, {new: true, runValidators: true})
+
+    if (!user) {
+      // Status 404:
+      throw new NotFound404(`Пользователь с указанным id: ${userId} не найден.`)
+    }
+    // Status 200:
+    res.send(user)
+
+  } catch (error) {
+    // Status 400:
+    if (error.name === 'ValidationError') {
+      return next(new BadRequest400('Переданы некорректные данные при обновлении профиля.'))
+    }
+    if (error.code === 11000) {
+      return next(new RequestConflict409('Пользователь с таким емайлом уже существует'))
+    }
+    // Status 500:
+    return next(error)
   }
-  const userId = req.user._id
-  return User.findByIdAndUpdate(userId, updData, {new: true, runValidators: true})
-    .then((user) => {
-      if (!user) {
-        // Status 404:
-        throw new NotFound404(`Пользователь с указанным id: ${userId} не найден.`)
-      }
-      // Status 200:
-      res.send(user)
-    })
-    .catch((error) => {
-      // Status 400:
-      if (error.name === 'ValidationError') {
-        return next(new BadRequest400('Переданы некорректные данные при обновлении профиля.'))
-      }
-      if (error.code === 11000) {
-        return next(new RequestConflict409('Пользователь с таким емайлом уже существует'))
-      }
-      // Status 500:
-      return next(error)
-    })
 }
 
 module.exports = {getCurrentUser, updateUser}
